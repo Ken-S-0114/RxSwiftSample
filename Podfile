7a1f904e8cf9248fe2b02d9a576ba6aaf0bc1c0f
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-01-22 01:26:43 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.2'
+
+use_frameworks!
+
+target 'RxSwiftSample' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+
+  # Pods for RxSwiftSample
+    pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+end
+
+# RxTests and RxBlocking make the most sense in the context of unit/integration tests
+target 'RxSwiftSampleTests' do
+    pod 'RxBlocking', '~> 4.0'
+    pod 'RxTest',     '~> 4.0'
+end
--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+let disposeBag = DisposeBag()
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var label1: UILabel!
+  
+  @IBOutlet weak var textField1: UITextField!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    textField1.rx.text.map{$0}.bind(to: label1.rx.text).disposed(by: disposeBag)
+ 
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
-
+  
 }
 
